773b62a83f12d91ba3bd1472a81010df7183b4d0
Ronald Arias <ariasron@hotmail.com>
2022-03-15 20:34:04 -0500

    uses prop getters for common use cases in a button or toggle

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,18 +7,31 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  const togglerProps = {'aria-pressed': on, onClick: toggle}
+
+  const getTogglerProps = (props = {}) => {
+    return {
+      ...props,
+      'aria-pressed': on,
+      onClick: props.onClick || toggle
+    }
+  }
   
-  return {on, togglerProps}
+  return {on, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
